bad26b71f27091146240f9eaa8facbf4b41542b0
Paul Dettorer Hervot <dettorer@dettorer.net>
2023-10-03 22:40:32 +0200

    new_feature: add a convenient wrapper around the VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,19 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 /// Panic handler that infinite loops since we're baremetal
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // ascii byte
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // color byte
-        }
-    }
+    println!("Hello, this is {} Test.", "a");
+    panic!("oh no!");
 
     loop {}
 }
