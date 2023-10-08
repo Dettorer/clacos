fcde20af500c0948262cd3137eb0b112dc2b5381
Paul Dettorer Hervot <dettorer@dettorer.net>
2023-10-08 16:31:46 +0200

    vga_buffer: add a few unit tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -142,3 +142,32 @@ macro_rules! println {
     () => ($crate::print!("\n"));
     ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_case]
+    fn test_println_simple() {
+        // checks that a simple print does not panic
+        println!("test_println_simple output");
+    }
+
+    #[test_case]
+    fn test_println_many() {
+        // checks that lots of prints do not panic
+        for _ in 0..200 {
+            println!("test_println_many output");
+        }
+    }
+
+    #[test_case]
+    fn test_println_output() {
+        let s = "Some test string that fits on a single line";
+        println!("{}", s);
+        for (i, c) in s.chars().enumerate() {
+            let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+            assert_eq!(char::from(screen_char.ascii_character), c);
+        }
+    }
+}
