7ac070d94ec00a0433943fa8eb92290578e3f02a
Paul Dettorer Hervot <dettorer@dettorer.net>
2023-10-23 11:55:13 +0200

    PIC: enable the PICs and prevent race conditions related to its timer interrupt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,17 +1,31 @@
 use lazy_static::lazy_static;
+use pic8259::ChainedPics;
+use spin;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use crate::{gdt, println};
+use crate::{gdt, print, println};
+
+// *************
+// * IDT setup *
+// *************
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
+
+        // breakpoint handler
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+
+        // double fault handler
         unsafe {
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+
+        // PIC timer interrupt handler
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+
         idt
     };
 }
@@ -19,6 +33,46 @@ lazy_static! {
 pub fn init_idt() {
     IDT.load();
 }
+
+// *************
+// * PIC setup *
+// *************
+
+/// The offset at which we will remap the PICs interrupts (the default offset starts at 0, which
+/// overlaps with the CPU exceptions)
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
+
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+// ************************
+// * CPU exceptions setup *
+// ************************
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
